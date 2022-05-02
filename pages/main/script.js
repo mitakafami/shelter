ec765dd1277bbf37e8a0b15f47b3a73594c86863
n1er4 <102894287+n1er4@users.noreply.github.com>
2022-05-02 18:09:38 +0400

    feat: add slider

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,1 +1,32 @@
+function btnOurFriends(e){
+    window.location.hash="ourFriends";
+    console.log("rame")
+}
+function buttonToOurFreinds(e){
+    window.location.hash="../pets/index.html"
+}
+const cardWrap = document.querySelector(".card-wrap");
+const card = document.getElementsByClassName("card");
 
+let counter = 0;
+
+const leftArrow = document.querySelector(".leftArrow");
+const rightArrow = document.querySelector(".rightArrow");
+
+leftArrow.addEventListener("click", ()=>{
+    if(counter == 0) {
+        counter=card.length-2
+    }if(counter !== 0){
+        counter--;
+    }
+    cardWrap.style.transform= 'translate('+(counter) *  -16.5 +'%)';
+});
+
+rightArrow.addEventListener('click',()=>{
+    if(counter == card.length-3){
+      counter=-1;
+  }if(counter <= card.length-4){
+      counter++
+  }
+  cardWrap.style.transform= 'translate('+(counter) *  -16.5 +'%)';
+})
